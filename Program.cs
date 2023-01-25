573b80da828c6e2ccd43ac970643a90aff839f4f
Velimira <revekka-best@mail.ru>
2023-01-25 21:32:06 +0300

    FillandPrintArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Создать двумерный массив:
+void FillArrayRandomNumbers2(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+// Напечатать двумерный массив:
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+int[,] matrix = new int[4,4];
+ FillArrayRandomNumbers2(matrix);
+ PrintArray(matrix);
