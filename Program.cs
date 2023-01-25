ba264f4ebe39ebbd09eec393e8b3207973c31784
Velimira <revekka-best@mail.ru>
2023-01-25 23:50:31 +0300

    Final

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ void FillArrayRandomLetters(string[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        System.Console.WriteLine($"Введите в столбик {i+1} строку массива, состоящую из {array.GetLength(0)} слов");
+        System.Console.WriteLine($"Введите {i+1} строку массива, состоящую из {array.GetLength(1)} слов/цифр:");
         for (int j = 0; j < array.GetLength(1); j++)
         {
             array[i, j] = Console.ReadLine();
@@ -25,6 +25,24 @@ void PrintArray(string[,] array)
         Console.WriteLine("");
     }
 }
-string [,] matrix = new string[3,2];
+void PrintShortArray(string[,] array, int n)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+     Console.Write("[ ");
+   for (int j = 0; j < array.GetLength(1); j++)
+   {
+   if (array[i,j].Length <= n)
+   {
+ Console.Write(array[i, j] + " ");
+   }
+   } 
+    Console.Write("]");
+        Console.WriteLine("");
+}
+}
+string [,] matrix = new string[2,3];
  FillArrayRandomLetters(matrix);
  PrintArray(matrix);
+ System.Console.WriteLine("Новый массив:");
+PrintShortArray(matrix,3);
